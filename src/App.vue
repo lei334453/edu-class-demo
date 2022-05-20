271070ce8cc297371afccb5f9cbf13674f856657
lei334453 <1834877626@qq.com>
2022-05-21 01:52:09 +0800

    two-commit-01

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
+<!--
+ * @Author: lei334453 1834877626@qq.com
+ * @Date: 2022-05-21 01:17:25
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-05-21 01:45:09
+ * @FilePath: \edu-class-demo\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+ <div id="app">
   <router-view/>
+   <h1>这还是哈哈</h1>
+ </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="scss" scoped>
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
